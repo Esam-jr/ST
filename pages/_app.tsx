1710eaacb9b98a5be75ce37500e04419cfd8fce5
Esmael <esmaelsabir9@gmail.com>
2025-05-06 10:13:57 +0300

    Add React Query setup and fix dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,27 @@ import { ThemeProvider } from 'next-themes'
 import { ThemeProvider as NextThemesProvider } from "@/components/ui/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 import RoleSelectionChecker from '@/components/auth/RoleSelectionChecker'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <RoleSelectionChecker>
-          <Component {...pageProps} />
-          <Toaster />
-        </RoleSelectionChecker>
-      </NextThemesProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <RoleSelectionChecker>
+            <Component {...pageProps} />
+            <Toaster />
+          </RoleSelectionChecker>
+        </NextThemesProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   )
-}
+} 
